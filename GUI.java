aa48a71c95c2ede54614457718377302f9396469
nikhil-naran <22wky1@queensu.ca>
2024-03-11 16:44:59 -0400

    initial

--- GUI.java
+++ GUI.java
@@ -1,12 +1,18 @@
 import javax.swing.*;
+import java.awt.*;
+
 public class GUI {
     public GUI(){
-        JFrame frame = new JFrame("GUI");
+        JFrame frame = new JFrame();
         JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+        panel.setLayout(new GridLayout(0,1));
 
-
-
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("GUI");
+        frame.pack();
+        frame.setVisible(true);
     }
 
 
